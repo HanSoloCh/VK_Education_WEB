bf3dc8eac9128fca971549b362aeb012eecda00b
Novikov Artem <novikovaa6@student.bmstu.ru>
2023-11-10 00:25:28 +0300

    add new filws

--- app/views.py
+++ app/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'question {i}',
+        'content': f'blablablablabla {i}',
+    } for i in range(30)
+]
+
+
+def paginate(objects, page_num, per_page=15):
+    paginator = Paginator(objects, per_page)
+
+    return paginator.page(page_num)
+
 
 def index(request):
-    return render(request, 'index.html')
+    page_num = request.GET.get('page', 1)
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page_num)})
 
 
-def question(request):
-    return render(request, 'question.html')
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question.html', {'question': item})
 
 
 def ask(request):
--- askme_novikov/urls.py
+++ askme_novikov/urls.py
@@ -19,8 +19,8 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('question', views.question),
+    path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('ask', views.ask),
     path('login', views.login),
     path('signup', views.signup),
